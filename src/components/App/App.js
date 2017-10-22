852ce0daa02f92d12aea6e54c65f23e23d44fdb2
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-10-22 08:26:22 +0300

    Change/Delete unnessesary elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
 
-import Igor from '../Igor/Igor'
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Igor helloString="Привет" />
+      <div className='App'>
+        <div className='App__wrapper'>
+          <div className='App__header'>
+            <Header />
+          </div>
+          <div className='App__main'>
+            <Main />
+          </div>
+          <div className='App__footer'>
+            <Footer />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
