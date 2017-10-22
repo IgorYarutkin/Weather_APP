2d17592cb055432b2774d11600525da0236de742
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-10-22 20:33:41 +0300

    Make GeoSwitch

---
+++ src/components/GeoSwitch/GeoSwitch.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './GeoSwitch.css';
+
+class GeoSwitch extends Component {
+  render() {
+    return (
+      <div className='GeoSwitch'>
+        Выбрать город
+      </div>
+    );
+  }
+}
+
+export default GeoSwitch; 
