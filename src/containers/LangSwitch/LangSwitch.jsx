49d0b229ac4992cab2012428b93a803c3bad2caa
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-10-23 06:39:19 +0300

    Add LangSwitch container

---
+++ src/containers/LangSwitch/LangSwitch.jsx
@@ -0,0 +1,32 @@
+import { connect } from  'react-redux';
+import DoubleSwitch from '../../components/DoubleSwitch/DoubleSwitch';
+import { changeLang } from "../../actions";
+
+const mapStateToProps = (state) => {
+   const { lang } = state;
+   const items = [];
+
+   for (let value in lang.availableLangs) {
+     const active = value === lang.currentLang;
+     const title = lang.availableLangs[value];
+
+     items.push({
+       value,
+       title,
+       active
+     });
+   }
+
+   return { items };
+}
+
+const mapDispatchToProps = dispatch => ({
+  onSelect: lang => dispatch(changeLang(lang))
+});
+
+const LangSwitch = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DoubleSwitch)
+
+export default LangSwitch;
